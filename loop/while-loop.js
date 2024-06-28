3041ed352c389dbda79c9c758ee68e5b5aa974b9
Dilruba Jannat Borna <bornadilruba38@gmail.com>
2024-06-28 23:38:53 +0300

    add

--- loop/while-loop.js
+++ loop/while-loop.js
@@ -1,5 +1,32 @@
+// example-1
 let n = 0;
 while (n < 5){
     n++;
 }
-console.log(n)
+// console.log(n)
+
+/**------------------------/** */
+
+//example-2
+
+let text = "";
+let i = 0;
+while (i < 6) {
+  text += i +
+  i++;
+}
+// console.log(i)
+
+/**------------------------/** */
+//example-3
+
+const cars = ["BMW", "Volvo", "Saab", "Ford"];
+let text2 = "";
+let x = 0;
+while (x < cars.length) {
+  text += cars[x] +
+  x++;
+}
+
+// console.log(x)
+// console.log(cars)

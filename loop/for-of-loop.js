903d8a122c16c05162396e7f139397ec85c45a41
Dilruba Jannat Borna <bornadilruba38@gmail.com>
2024-07-05 21:05:37 +0300

    add

---
+++ loop/for-of-loop.js
@@ -0,0 +1,31 @@
+const cars = ["BMW", "Volvo", "Mini"];
+
+let text = "";
+for (let x of cars) {
+  text += x + " ";
+}
+
+
+// console.log(text)
+
+
+
+/*------------------------------*/
+
+// array
+const students = ['Dilruba', 'Jannat', 'Borna'];
+
+// using for...of
+for ( let element of students ) {
+
+// display the values
+    // console.log(element);
+}
+
+/*------------------------------*/
+
+const array = [ 1, 2, 3, 4, 5 ];
+
+for (const item of array) {
+    console.log(item);
+}

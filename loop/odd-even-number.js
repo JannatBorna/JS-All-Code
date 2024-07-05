7dbf863a2397b93da7a07942cee151569b505eb8
Dilruba Jannat Borna <bornadilruba38@gmail.com>
2024-07-05 21:20:33 +0300

    add

---
+++ loop/odd-even-number.js
@@ -0,0 +1,31 @@
+// Using a for loop to iterate through numbers from 0 to 15
+for (var x = 0; x <= 15; x++) {
+    // Checking if the current number is 0
+    if (x === 0) {
+        // console.log(x + " is even");
+    }
+    // Checking if the current number is even
+    else if (x % 2 === 0) {
+        // console.log(x + " is even");
+    }
+    // If the number is not 0 and not even, it is odd
+    else {
+        // console.log(x + " is odd");
+    }
+} 
+
+/*------------------------------*/
+
+let nums = 22;
+for (let i = 0; i < nums; i++){
+   let  b = i * 2;
+    if (b <= nums){
+        if (b == 0) {
+
+        } else 
+
+        { 
+            console.log('Count',b);
+        }
+    };       
+};
